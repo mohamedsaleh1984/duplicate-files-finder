b0ad6927cd36ddfdedd40e4559a476b89130120a
mohamed.saleh <mohamedsaleh1984@hotmail.com>
2025-03-09 17:50:13 -0400

    debug

--- main.cpp
+++ main.cpp
@@ -13,12 +13,38 @@
 #include "xxhash/xxhash.h"
 using namespace std;
 using namespace ns_finder;
+#define RELEASE
 
 int main(int argc, char **argv)
 {
+#ifdef DEBUG
     Finder fin;
-    fs::path root = "D:\\TEMP_DUP_DATA";
+    fs::path root = "D:\\____c";
     fin.start_search(root);
+#endif
+
+#ifdef RELEASE
+    bool bfetch = true;
+
+    while (bfetch)
+    {
+        if (argc == 2)
+        {
+            bfetch = false;
+            string programName = argv[0];
+            string location = argv[1];
+
+            Finder fin;
+            fs::path root = argv[1];
+            fin.start_search(root);
+        }
+        else
+        {
+            std::cerr << "Usage: program dir_location\n";
+            bfetch = false;
+        }
+    }
+#endif
 
     return 0;
 }
